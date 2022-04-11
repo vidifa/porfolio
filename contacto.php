aadd651fc7bdc4ce1c4fdf3559c6ccf19496d393
delivereesapp@gmail.com <delivereesapp@gmail.com>
2022-04-11 20:57:50 +0200

    cambios de archivos a php

--- contacto.php
+++ contacto.php
@@ -24,16 +24,16 @@
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="index.html">Inicio</a>
+                            <a class="nav-link" aria-current="page" href="index.php">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="sobre-mi.html">Sobre mi</a>
+                            <a class="nav-link" href="sobre-mi.php">Sobre mi</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="proyectos.html">Proyectos</a>
+                            <a class="nav-link" href="proyectos.php">Proyectos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" href="contacto.html">Contactos</a>
+                            <a class="nav-link active" href="contacto.php">Contactos</a>
                         </li>
                     </ul>
                     <div class="d-flex">
